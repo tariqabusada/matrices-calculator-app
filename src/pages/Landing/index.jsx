cc9b4346d2c7065ddde50882d742f4476e10fa0e
tariqabusada <tariqesa101@gmail.com>
2022-12-21 14:29:14 +0200

    ...

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -7,7 +7,7 @@ export default function Landing (){
         <div className="landing">
             <Logo/>
             <div className="start-sec">
-                <img className='land-img' src='https://i.imgur.com/mGkHi1e.png' title="maths sticker" alt="maths"></img>
+                <img className='land-img' src='https://i.imgur.com/mGkHi1e.png' title="maths sticker" alt="maths" />
                 <p>The first step to getting your site on Google is to be sure that Google can find it. The best way to do that is to submit a sitemap. A sitemap is a file on your site that tells search engines about new or changed pages on your site. Learn more about how to build and submit a sitemap.</p>
                 <Link to={'/calculator'}><button>Let's gooo! 😸</button></Link>
             </div>
